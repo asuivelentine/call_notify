3d1b36d338600e0a85d8d8773719544d87b1ae8b
Kai Sickeler <k.sickeler@gmail.com>
2016-02-25 13:12:36 +0100

    more unit structure

--- src/msg_handler.rs
+++ src/msg_handler.rs
@@ -1,14 +1,24 @@
 use msg::Message;
+use std::sync::mpsc::{ Sender, Receiver };
+use std::sync::mpsc::channel;
 
 trait NotificationListener {
     fn message_received(&self, msg: Message) -> Message;
     fn connectionClosed(&self);
 }
 
-struct MessageHandler;
+struct MessageHandler<'a> {
+    listeners: Vec<&'a NotificationListener>,
+}
 
-impl MessageHandler {
+impl<'a> MessageHandler<'a> {
+    pub fn connect(port: u16) {
+        let (tx, rx): (Sender<String>, Receiver<String>) = channel();
 
+    }
+    pub fn register(&self, item: &NotificationListener) {
+
+    }
 }
 
 #[cfg(test)]
