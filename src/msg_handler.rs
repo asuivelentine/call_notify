eece7206c829ea242ebdf84334a4810559399555
Kai Sickeler <k.sickeler@gmail.com>
2016-02-25 12:01:29 +0100

    definied initial structure

--- src/msg_handler.rs
+++ src/msg_handler.rs
@@ -1,6 +1,17 @@
 use msg::Message;
 
 trait NotificationListener {
-    fn message_received(msg: Message) -> Message;
-    fn connectionClosed();
+    fn message_received(&self, msg: Message) -> Message;
+    fn connectionClosed(&self);
+}
+
+struct MessageHandler;
+
+impl MessageHandler {
+
+}
+
+#[cfg(test)]
+mod test {
+
 }
